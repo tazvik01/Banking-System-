9fa0f9bc77460443553a35e8396ee39394f5e1e9
tazvik01 <tazvikziauddin@gmail.com>
2022-05-04 18:36:53 -0600

    Added the second class and created deposit function

--- bankingsystem.py
+++ bankingsystem.py
@@ -3,3 +3,49 @@ class User():
         self.name = name
         self.gender = gender
         self.age = age
+
+    name = input("What is your name:")
+    gender = input("What is your gender:")
+    age = input("What is your age:")
+
+    print("")
+    print("Account Details")
+    print("---------------")
+    print("")
+    print("Name:", name)
+    print("Gender:", gender)
+    print("Age:", age)
+    print("")
+    print("---------------")
+    
+
+    
+
+
+
+class Bank(User):
+    
+    def __init__(self,name,gender,age):
+        super().__init__(name,gender,age)
+        
+    
+
+    def deposit(self,amount,balance):
+        self.amount = amount
+        self.balance = balance
+
+    amount = int(input("How much money do you want to deposit: "))
+    balance = 0
+    balance += amount
+    print("Your current balance is $", balance)
+
+    
+
+
+
+
+
+
+    
+
+    
